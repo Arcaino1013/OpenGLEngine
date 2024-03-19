de977aacdb0248607bc5465f5bfc62a9a6b0e1ac
Aeneas Reina <aeneasreina@gmail.com>
2024-03-19 20:52:43 +0100

    Working window

--- VulkanEngine/OpenGLEngine/main.cpp
+++ VulkanEngine/OpenGLEngine/main.cpp
@@ -8,28 +8,25 @@
 
 #include <iostream>
 
-int main() {
-    glfwInit();
+#include "first_app.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+int main() 
+{
+    lve::FirstApp app{};
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    try 
+    {
+        app.run();
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    glfwDestroyWindow(window);
 
-    glfwTerminate();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
